1780e8082cba44aef572a3d9e1f721dfe8714073
kalodoukas <37884563+kalodoukas@users.noreply.github.com>
2026-06-08 14:09:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'starting-valve-v24';
+const ASSETS = [
+  './',
+  './index.html?v=24',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : undefined))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html?v=24')))
+  );
+});
